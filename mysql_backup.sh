c4a527bcca4c35f415cad403ad71a744678cbd67
Tom.Li <open222333@gmail.com>
2023-01-03 16:53:26 +0800

    修改 mysql備份

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -28,7 +28,7 @@ TAR_BAK="mysqldb_bak_$HOSTNAME-$DATE.tar"
 
 # 備份全部數據 若有帳密 則執行有帳密的指令
 if [ $MYSQLDB_USER ]; then
-	xtrabackup --user=$MYSQLDB_USER --password=$MYSQLDB_PASS --backup --target-dir=$TAR_DIR
+	xtrabackup --user=$MYSQLDB_USER --password=$MYSQLDB_PASS --backup --target-dir=$OUT_DIR/$DATE
 else
 	echo "需要帳號密碼"
 fi
@@ -48,6 +48,25 @@ fi
 # 刪除tar備份包$DAYS天前的備份文件
 find $TAR_DIR/ -mtime +$DAYS -name "*.tar" -exec rm -rf {} \;
 
-rsync -Pav -e ssh $TAR_DIR/$TAR_BAK root@$HOST:$TARGET_DIR
+# 使用key
+if [ $USE_KEY ]; then
+	if [ $USE_KEY == 1 ]; then
+		if [[ ! -e $HOME/.ssh/$KEYNAME ]]; then
+			sh `dirname -- "$0"`/generate_ssh_key.sh
+		fi
+	fi
+fi
+
+# 自動
+if [ $AUTO_PASSWORD ]; then
+	if [ $AUTO_PASSWORD == 1 ]; then
+		if ! [ -x "$(command -v sshpass)" ]; then
+			sh `dirname -- "$0"`/tool_install.sh sshpass
+		fi
+		rsync -Pav -e "sshpass -p$HOST_PASSWORD ssh" $TAR_DIR/$TAR_BAK root@$HOST:$TARGET_DIR
+	else
+		rsync -Pav -e ssh $TAR_DIR/$TAR_BAK root@$HOST:$TARGET_DIR
+	fi
+fi
 
 exit
